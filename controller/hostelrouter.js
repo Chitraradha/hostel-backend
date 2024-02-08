d205a8e801b1111cd578a5a93ab954a34e06fb0d
Chitraradha <chitrak1110@gmail.com>
2024-02-08 22:27:15 +0530

    open

--- controller/hostelrouter.js
+++ controller/hostelrouter.js
@@ -2,5 +2,13 @@ const express=require("express")
 const hostelModel=require("../model/hostelmodel")
 const router=express.Router()
 
+router.post("/add",async(req,res)=>{
+    let data=req.body
+    let hostel=new hostelModel(data)
+    let result=await hostel.save()
+    res.json({
+        status:"success"
+    })
+})
 
 module.exports=router
